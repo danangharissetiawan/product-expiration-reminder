26e7f43e606da1c52303fc44b2a72638667f1cfc
Darisset <111202013179@mhs.dinus.ac.id>
2022-11-24 20:11:46 +0700

    update

--- apps/dashboard/utils/check_camera.py
+++ apps/dashboard/utils/check_camera.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 def return_camera_indexes():
     # checks the first 10 indexes.
     index = 0
@@ -13,5 +14,6 @@ def return_camera_indexes():
         index += 1
         i -= 1
     return arr
+
 
 print('Available camera indexes:', return_camera_indexes())
